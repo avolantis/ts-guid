18b9322199298753760b7e46cdefbdabcd03a8c8
Bence Lang <bence.lang@avolantis.net>
2020-11-27 01:12:23 +0100

    test: fixed missing coverage (#19)

--- config/jest.js
+++ config/jest.js
@@ -1,14 +1,15 @@
 const { resolve } = require("path");
 
 module.exports = {
-  roots: [resolve(__dirname, "../tests")],
+  rootDir: resolve(__dirname, "../"),
   preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverage: true,
-  coverageDirectory: resolve(__dirname, "../coverage"),
+  testEnvironment: process.env.TEST_ENVIRONMENT,
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+
+  coverageDirectory: "<rootDir>/coverage",
   globals: {
-    "ts-jest": {
-      babelConfig: resolve(__dirname, `./babel.browser.js`)
-    }
+    "ts-jest": {},
+    "window": process.env.TEST_ENVIRONMENT === "node" ? null : undefined,
+    "self": process.env.TEST_ENVIRONMENT === "node" ? null : undefined
   }
 };
