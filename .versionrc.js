ba74286812530411889d22c5d75aa46ff28297c4
Bence Lang <bence.lang@avolantis.net>
2021-03-10 20:22:38 +0100

    chore: auto bump version in readme (#26)

--- .versionrc.js
+++ .versionrc.js
@@ -1,4 +1,10 @@
 const types = require("./config/commit");
 module.exports = {
+  bumpFiles: [
+    {
+      filename: "README.md",
+      updater: require("./config/version-updater")
+    }
+  ],
   types
 };
